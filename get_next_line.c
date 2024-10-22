e6c956326b2d9252c2a87c2b1ad6646fbe60ecca
AbdallahZerfaoui <82400944+AbdallahZerfaoui@users.noreply.github.com>
2024-10-22 18:39:31 +0200

    V7 42
    Clean version

--- get_next_line.c
+++ get_next_line.c
@@ -6,93 +6,37 @@
 /*   By: azerfaou <azerfaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 12:28:32 by azerfaou          #+#    #+#             */
-/*   Updated: 2024/10/22 12:24:40 by azerfaou         ###   ########.fr       */
+/*   Updated: 2024/10/22 18:35:30 by azerfaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	gnl_strlcpy(char *dst, char *src, size_t dstsize)
-{
-	unsigned long	i;
-
-	i = 0;
-	if (dstsize == 0)
-		return (ft_strlen(src));
-	while (i < dstsize - 1 && src[i])
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (ft_strlen(src));
-}
-
-// void	gnl_bzero(void *s, size_t n)
-// {
-// 	unsigned char	*d;
-
-// 	d = s;
-// 	while (0 < n)
-// 		d[n-- - 1] = 0;
-// }
-
-// char	*ft_strjoin_gnl(char const *s1, char const *s2)
-// {
-// 	int		i;
-// 	int		j;
-// 	char	*ptr;
-
-// 	i = -1;
-// 	j = -1;
-// 	if (s1 == NULL)
-// 		return (gnl_bzero((char *)s2, ft_strlen(s2)), NULL);
-// 	ptr = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-// 	if (ptr == NULL)
-// 		return (NULL);
-// 	while (s1 && s1[++i])
-// 		ptr[i] = s1[i];
-// 	while (s2 && s2[++j])
-// 		ptr[i + j] = s2[j];
-// 	ptr[i + j] = '\0';
-// 	free(s1);
-// 	s1 = NULL;
-// 	return (ptr);
-// }
-
-char	*ft_strjoin_gnl(char const *s1, char const *s2)
+static char	*ft_strjoin_gnl(char const *s1, char const *s2)
 {
 	char	*str_result;
 
-	// if (!s1 && !s2)
-	// 	return (NULL);
 	if (!s1)
 		return (memset((char *)s2, 0, ft_strlen(s2)), NULL);
 	str_result = (char *)ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1,
 			sizeof(char));
-	// str_result = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	// fprintf(file, "str_result from join: %p\n", str_result);
 	if (!str_result)
 		return (NULL);
 	if (s1 && *s1)
 		ft_strncpy(str_result, s1, ft_strlen(s1));
-		// gnl_strlcpy(str_result, (char *)s1, ft_strlen(s1));
 	if (s2 && *s2)
 		ft_strncpy(str_result + ft_strlen(s1), s2, ft_strlen(s2));
-		// gnl_strlcpy(str_result + ft_strlen(s1), (char *)s2, ft_strlen(s2));
-	// str_result[ft_strlen(s1) + ft_strlen(s2)] = '\0';
 	return (str_result);
 }
 
-static char	*extract_and_update(char *new_line_ptr,
-		char **remainder)
+static char	*extract_and_update(char *new_line_ptr, char **remainder)
 {
 	size_t	line_len;
 	char	*new_remainder;
-	char	*line = NULL;
+	char	*line;
 
 	new_remainder = NULL;
-
+	line = NULL;
 	line_len = new_line_ptr - *remainder + 1;
 	if (line_len >= 0)
 		line = (char *)ft_calloc(line_len + 1, sizeof(char));
@@ -103,7 +47,8 @@ static char	*extract_and_update(char *new_line_ptr,
 	{
 		new_remainder = ft_strdup(new_line_ptr + 1);
 		if (!new_remainder)
-			return (free(line), free(new_remainder), new_remainder = NULL, NULL);
+			return (free(line), free(new_remainder), new_remainder = NULL,
+				NULL);
 	}
 	free(*remainder);
 	*remainder = new_remainder;
@@ -117,40 +62,27 @@ static ssize_t	read_and_append(int fd, char **remainder)
 	char	*new_remainder;
 
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read < 0)
-		return (free(*remainder), *remainder = NULL, -1);
-	if (bytes_read > 0)
+	buffer[bytes_read] = '\0';
+	if (!(*remainder))
 	{
-		buffer[bytes_read] = '\0';
-		if (!(*remainder))
+		*remainder = ft_strdup(buffer);
+		if (!*remainder)
+			return (-1);
+	}
+	else
+	{
+		new_remainder = ft_strjoin_gnl(*remainder, buffer);
+		if (!new_remainder && *remainder && **remainder)
+			return (free(*remainder), *remainder = NULL, -1);
+		else if (*remainder && **remainder)
 		{
-			*remainder = ft_strdup(buffer);
-			if (!*remainder)
-				return (-1);
-		}
-		else
-		{
-			new_remainder = ft_strjoin_gnl(*remainder, buffer);
-			if (!new_remainder)
-			{
-				if (*remainder && **remainder)
-				{
-					// free(new_remainder);
-					free(*remainder);
-					*remainder = NULL;
-					new_remainder = NULL;
-				}
-				return (-1);
-			}
-			else if (*remainder && **remainder)
-			{
-				free(*remainder);
-				*remainder = new_remainder;
-			}
+			free(*remainder);
+			*remainder = new_remainder;
 		}
 	}
 	return (bytes_read);
 }
+
 /***
  * @brief this function handles the end of file
  * @param line the line to be returned
@@ -182,15 +114,15 @@ static void	handle_eof(char **line, char **remainder)
  */
 char	*get_next_line(int fd)
 {
-	ssize_t	bytes_read;
-	char	*new_line_ptr = NULL;
-	char	*line = NULL; //to remove if not needed
-	static char *remainder = NULL;
+	ssize_t		bytes_read;
+	char		*new_line_ptr;
+	char		*line;
+	static char	*remainder = NULL;
 
+	line = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
-		return (free(remainder), free(line), remainder = NULL, line = NULL, NULL);
-		// return(gnl_bzero(remainder, ft_strlen(remainder)), NULL);
-	while(1)
+		return (free(remainder), remainder = NULL, NULL);
+	while (1)
 	{
 		bytes_read = read_and_append(fd, &remainder);
 		if (bytes_read < 0)
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: azerfaou <azerfaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 16:40:44 by azerfaou          #+#    #+#             */
-/*   Updated: 2024/10/16 16:49:23 by azerfaou         ###   ########.fr       */
+/*   Updated: 2024/10/22 18:30:19 by azerfaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,17 @@
 #  define BUFFER_SIZE 10
 # endif
 
-# include <stdlib.h>
-# include <unistd.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
-// Standard functions (9)
+// Standard functions
 char	*get_next_line(int fd);
 char	*ft_strdup(const char *s);
-// size_t	ft_strlcat_gnl(char *dst, const char *src, size_t dst_size);
 size_t	ft_strlen(const char *s);
-// int		ft_strcmp(const char *s1, const char *s2);
-// void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strchr(const char *str, int search_chr);
 char	*ft_strncpy(char *destination, const char *source, size_t n);
-// size_t	ft_strlcpy(char *dst, const char *src, size_t dst_size);
-char	*ft_strjoin_gnl(char const *s1, char const *s2);
 void	*ft_calloc(size_t nitems, size_t size);
-
-// Helper functions (3)
-// char	*extract_line_and_update_remainder(char *new_line_ptr,
-// 			char **remainder);
-// ssize_t	read_and_append_remainder(int fd, char **remainder);
-// void	handle_EOF(char **line, char **remainder);
 
 #endif
